9150382c8e46a4e69ac5d338a68da81a679f0dd7
Alex Sandro <alexsandro49.dev@gmail.com>
2025-08-01 22:21:18 -0300

    feat: district model

---
+++ district/models.py
@@ -0,0 +1,19 @@
+import uuid
+from django.db import models
+from states.models import State
+from municipalities.models import Municipality
+
+class District(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    integration_id = models.IntegerField()
+    name = models.CharField()
+    state_id = models.ForeignKey(State, on_delete=models.CASCADE)
+    municipality_id = models.ForeignKey(Municipality, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'district_ibge'
+
+    def __str__(self):
+        return f'name: {self.name}'
